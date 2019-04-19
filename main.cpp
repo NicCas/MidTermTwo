93a394cfbd528ce94b1501702377cfaa7bab390a
Nicole C <nicas@sonic.net>
2019-04-18 17:46:21 -0700

    Human set up

--- main.cpp
+++ main.cpp
@@ -1,4 +1,45 @@
 #include <iostream>
+#include <cstdio>
+#include <string>
+
+// Base class
+class Human
+{
+private:
+    // Declare base class var
+    std::string _name;
+    int _age;
+    char _sex;
+
+    // Base class constructor
+    Human();
+
+protected:
+    // Protected constructor for name age sex
+    Human (const std::string & n, const int & a, const char & s) : _name(n), _age(a), _sex(s)
+    {
+        std::cout<<"Human Constructed\n";
+    }
+
+    ~Human()
+    {
+        std::cout<< "Destructing\n";
+    }
+
+public:
+    const std::string & name() const {return _name;}
+    const int & age() const {return _age;}
+    const char & sex() const {return _sex;}
+
+};
+
+class Parent {
+
+};
+
+class Child {
+
+};
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
